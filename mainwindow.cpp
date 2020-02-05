da6ed77d853a0bb342e72137a7a97d26e4a903c4
Murat Tunç <murat.tunc8558@gmail.com>
2020-02-05 12:42:59 +0000

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,4 @@
-#include <opencv4/opencv2/core/core.hpp>
+﻿#include <opencv4/opencv2/core/core.hpp>
 #include <opencv4/opencv2/highgui/highgui.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/imgcodecs/imgcodecs.hpp>
@@ -6,8 +6,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "stdio.h"
-#include<QDebug>
-#include<string>
+#include <QDebug>
+#include <string>
 
 #include <vector>
 #include <algorithm>
@@ -17,28 +17,14 @@ using namespace std;
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    namedWindow("T1");
-    //*********************************************************//
-    img_BGR=imread("Road_Snap2.png");
-    cvtColor(img_BGR, img_RGB,COLOR_BGR2RGB); //convert to RGB
-    cvtColor(img_BGR, img_GRAY,COLOR_BGR2GRAY); //convert to GRAY
-    cvtColor(img_BGR, img_HSV,COLOR_BGR2HSV); //convert to GRAY;
-
-
-    imgWidth=img_BGR.cols;
-    imgHeight=img_BGR.rows;
-
-    //qDebug()<<imgHeight;
-
-
-    Birds_Eye_View();
 
 
 
-    //Adaptive_Threshold();
-    //Threshold_RGB_White();
-    //Hough_Transform();
-    //HSV_Threshold();
+    Initialize();
+    Calibrate();
+   // Birds_Eye_View();
+    //Detect_Lane();
+
 
 
     //ui->image_lbl->setPixmap(QPixmap::fromImage(QImage(img_HSV.data, img_HSV.cols, img_HSV.rows, img_HSV.step, QImage::Format_RGB444)));
@@ -49,21 +35,158 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-void MainWindow::Birds_Eye_View()
+void MainWindow::Initialize()
+{
+    namedWindow("T1");
+    //*********************************************************//
+    img_BGR=imread("Road_Snap5.png");
+    cvtColor(img_BGR, img_RGB,COLOR_BGR2RGB); //convert to RGB
+
+    imgWidth=img_BGR.cols;
+    imgHeight=img_BGR.rows;
+
+}
+void MainWindow::Calibrate(){
+
+    vector<string> filelist;
+    string windowName="Detected points";
+
+    // generate list of calibration image filename
+    // named calibration1 to chessboard20 in camera_cal sub-direction
+    for (int i=1; i<=20; i++) {
+
+        stringstream str;
+        str << "camera_cal/calibration" << i << ".jpg";
+        filelist.push_back(str.str());
+
+    }
+
+    Size boardSize(9,6);
+    // the points on the chessboard
+    vector<Point2f> imageCorners;
+    vector<Point3f> objectCorners;
+
+    // 3D Scene Points:
+    // Initialize the chessboard corners
+    // in the chessboard reference frame
+    // The corners are at 3D location (X,Y,Z)= (i,j,0)
+    for (int i=0; i<boardSize.height; i++) {
+        for (int j=0; j<boardSize.width; j++) {
+
+            objectCorners.push_back(Point3f(i, j, 0.0f));
+        }
+    }
+
+    // 2D Image points:
+    Mat image; // to contain chessboard image
+    int successes = 0;
+    // for all viewpoints
+    for (int i=0; i<filelist.size(); i++) {
+
+        // Open the image
+        image = imread(filelist[i],0);
+
+        // Get the chessboard corners
+        bool found = findChessboardCorners(image,         // image of chessboard pattern
+                                               boardSize,     // size of pattern
+                                               imageCorners); // list of detected corners
+
+        // Get subpixel accuracy on the corners
+        if (found) {
+            cornerSubPix(image, imageCorners,
+                             cv::Size(5, 5), // half size of serach window
+                             cv::Size(-1, -1),
+                             cv::TermCriteria(cv::TermCriteria::MAX_ITER +
+                                              cv::TermCriteria::EPS,
+                                              30,		// max number of iterations
+                                              0.1));  // min accuracy
+
+            // If we have a good board, add it to our data
+            if (imageCorners.size() == boardSize.area()) {
+                imagePoints.push_back(imageCorners);     // 2D image points from one view
+                objectPoints.push_back(objectCorners);  // corresponding 3D scene points
+                successes++;
+            }
+        }
+
+        if (windowName.length()>0 && imageCorners.size() == boardSize.area()) {
+
+            //Draw the corners
+            cv::drawChessboardCorners(image, boardSize, imageCorners, found);
+            cv::imshow(windowName, image);
+            cv::waitKey(3000);
+        }
+    }
+
+
+}
+
+
+
+void MainWindow::Detect_Lane()
 {
 
+        //3-1-Binary Convertion
+        //Declare the output variables
+        Mat dst, cdst, cdstP;
+        Mat src= img_Wrap.clone();
 
-    qint16 X_Top_Left=560;
-    qint16 Y_Top_Left=415;
+        cvtColor(img_Wrap,dst,COLOR_RGB2GRAY); //convert to GRAYSCALE
+        cvtColor(img_Wrap,cdstP,COLOR_RGB2GRAY); //convert to GRAYSCALE
+        // Edge detection
+        qint16 Threshold_1=50;
+        qint16 Threshold_2=220;
+        Canny(src, dst, Threshold_1, Threshold_2, 3);
+        imshow("Canny", dst);
 
-    qint16 X_Top_Right=710;
-    qint16 Y_Top_Right=415;
+//        // Standard Hough Line Transform
+//        vector<Vec2f> lines; // will hold the results of the detection
+//        HoughLines(dst, lines, 1, CV_PI/180, 250, 0, 0 ); // runs the actual detection
+//        // Draw the lines
+//        for( size_t i = 0; i < lines.size(); i++ )
+//        {
+//            float rho = lines[i][0], theta = lines[i][1];
+//            Point pt1, pt2;
+//            double a = cos(theta), b = sin(theta);
+//            double x0 = a*rho, y0 = b*rho;
+//            pt1.x = cvRound(x0 + 1000*(-b));
+//            pt1.y = cvRound(y0 + 1000*(a));
+//            pt2.x = cvRound(x0 - 1000*(-b));
+//            pt2.y = cvRound(y0 - 1000*(a));
+//            line( dst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+//        }
+//        imshow("Hough Transform", dst);
 
-    qint16 X_Bottom_Right=940;
-    qint16 Y_Bottom_Right=600;
 
-    qint16 X_Bottom_Left=340;
-    qint16 Y_Bottom_Left=600;
+        Mat maskWhite;
+        qint16 Threshold=200;
+        inRange(img_Wrap, Scalar(Threshold, Threshold, Threshold), Scalar(255, 255, 255), maskWhite);
+        imshow("maskWhite", maskWhite);
+
+
+
+
+
+
+
+}
+
+
+void MainWindow::Birds_Eye_View()
+{
+    //2-1-Define Source & Destination Points
+
+    qint16 X_Top_Left=590;
+    qint16 Y_Top_Left=450;
+
+    qint16 X_Top_Right=690;
+    qint16 Y_Top_Right=450;
+
+    qint16 X_Bottom_Right=1060;
+    qint16 Y_Bottom_Right=650;
+
+    qint16 X_Bottom_Left=300;
+    qint16 Y_Bottom_Left=650;
 
     qint16 Dst_diff=20;
 
@@ -75,10 +198,10 @@ void MainWindow::Birds_Eye_View()
     };
 
     Point2f dst[]={
-        Point2f(0,0),
-        Point2f(640,0),
-        Point2f(640,480),
-        Point2f(0,480),
+        Point2f(250,0),
+        Point2f(896,0),
+        Point2f(896,720),
+        Point2f(250,720),
     };
     Point PT1,PT2,PT3,PT4;
     PT1.x=X_Top_Left;
@@ -93,24 +216,25 @@ void MainWindow::Birds_Eye_View()
     PT4.x=X_Bottom_Left;
     PT4.y=Y_Bottom_Left;
 
+    //2-2-Draw Rectangle
     line(img_BGR,PT1,PT2,Scalar( 255, 0, 0 ),1,LINE_8,0);
     line(img_BGR,PT2,PT3,Scalar( 255, 0, 0 ),1,LINE_8,0);
     line(img_BGR,PT3,PT4,Scalar( 255, 0, 0 ),1,LINE_8,0);
     line(img_BGR,PT4,PT1,Scalar( 255, 0, 0 ),1,LINE_8,0);
 
     imshow("Original Image", img_BGR);
+    //**********************************************************//
 
-
+    //2-3-Warp Perspective Image
     //Prepare the matrix for transform and get the warped image.
 
     Mat perspectiveMatrix=getPerspectiveTransform(src,dst);
-    Mat img_Wrap(480, 640, CV_8UC3); //Destination for warped image
+    img_Wrap=Mat(480, 640, CV_8UC3); //Destination for warped image
     Mat invertedPerspectiveMatrix;
     invert(perspectiveMatrix, invertedPerspectiveMatrix);
     warpPerspective(img_RGB, img_Wrap, perspectiveMatrix, img_Wrap.size(), INTER_LINEAR, BORDER_CONSTANT);
 
     imshow("warpPerspective",img_Wrap);
-
 
 
 
@@ -162,6 +286,7 @@ void MainWindow::Hough_Transform()
     qint16 Threshold_2=250;
     Canny(img_GRAY, img_dst, Threshold_1, Threshold_2, 3);
 
+    imshow("T1", img_dst);
     // Copy edges to the images that will display the results in BGR
     cvtColor(img_dst, img_cdst, COLOR_GRAY2BGR);
     img_cdstP = img_cdst.clone();
@@ -196,8 +321,6 @@ void MainWindow::Hough_Transform()
     }
 
     imshow("Hough Result",img_cdstP);
-
-
 
 }
 
---
+++ mainwindow.h
@@ -0,0 +1,62 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <opencv4/opencv2/opencv.hpp>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+using namespace std;
+using namespace cv;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void Threshold_RGB_White();
+    void Threshold_RGB_Yellow();
+    void Hough_Transform();
+    void HSV_Threshold();
+    void Adaptive_Threshold();
+    void Vertical_Lines();
+    void show_wait_destroy(const char* winname, Mat img);
+    void Birds_Eye_View();
+    void Initialize();
+    void Detect_Lane();
+    void Calibrate();
+
+
+
+
+
+private slots:
+
+    
+private:
+    Ui::MainWindow *ui;
+    Mat img_BGR;
+    Mat img_RGB;
+    Mat img_GRAY;
+    Mat img_HSV;
+    Mat img_dst;
+    Mat img_cdst;
+    Mat img_cdstP;
+    Mat img_Binary;
+    Mat lower_red_hue_range;
+    Mat upper_red_hue_range;
+    Mat red_hue_image;
+    Mat img_Wrap;
+    qint16 imgWidth;
+    qint16 imgHeight;
+
+    vector<vector<Point3f> > objectPoints;     // the image point positions in pixels
+    vector<vector<Point2f> > imagePoints;     // output Matrices
+    Mat cameraMatrix;
+    Mat distCoeffs;
+};
+
+#endif // MAINWINDOW_H
